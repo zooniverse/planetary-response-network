f86314d415c120c86a7df6b90826f314e8bd3457
Tom Jenkins <tom@itsravenous.com>
2016-01-21 10:19:46 +0000

    Stick pxToGeo in own module

--- planet-api-before-after-test.js
+++ planet-api-before-after-test.js
@@ -7,6 +7,7 @@ var exec      = require('child_process').exec
 var async     = require('async')
 var planetAPI = require('./modules/planet-api.js')
 var geoCoords = require('./modules/geo-coords.js')
+var pxToGeo = require('./modules/px-to-geo.js')
 
 
 var im           = require('imagemagick')
@@ -154,14 +155,7 @@ function tilizeImage(filename, tileSize, overlap, callback){
 // for debugging: edge of runway at Kathmandu airport
 // console.log( pxToGeo( 2582,3406, size.x, size.y, reference_coordinates ) );
 
-/* Convert corner and center pixels to geographical coordinates */
-function pxToGeo( x, y, wid, hei, reference_coordinates ){
-  delta_lon = Math.abs( reference_coordinates.upper_right.lon - reference_coordinates.upper_left.lon )
-  delta_lat = Math.abs( reference_coordinates.upper_right.lat - reference_coordinates.bottom_right.lat )
-  offset_lon = x * delta_lon / wid;
-  offset_lat = y * delta_lat / hei;
-  return {lon: reference_coordinates.upper_left.lon + offset_lon, lat: reference_coordinates.upper_right.lat - offset_lat}
-}
+
 
 
 /* Methods to read/write JSON metadata using exiv2 */
--- tilize-images.js
+++ tilize-images.js
@@ -7,6 +7,7 @@ var async        = require('async')
 var path         = require('path')
 var yargs        = require('yargs')
 var exiv2        = require('exiv2')
+var pxToGeo      = require('./modules/px-to-geo')
 
 var argv = yargs
     .usage('$0 [options] filename')
@@ -94,15 +95,6 @@ async.series(task_list, function (error, result) {
 
 // for debugging: edge of runway at Kathmandu airport
 // console.log( pxToGeo( 2582,3406, size.x, size.y, reference_coordinates ) );
-
-/* Convert corner and center pixels to geographical coordinates */
-function pxToGeo( x, y, wid, hei, reference_coordinates ){
-  delta_lon = Math.abs( reference_coordinates.upper_right.lon - reference_coordinates.upper_left.lon )
-  delta_lat = Math.abs( reference_coordinates.upper_right.lat - reference_coordinates.bottom_right.lat )
-  offset_lon = x * delta_lon / wid;
-  offset_lat = y * delta_lat / hei;
-  return {lon: reference_coordinates.upper_left.lon + offset_lon, lat: reference_coordinates.upper_right.lat - offset_lat}
-}
 
 
 /* Methods to read/write JSON metadata using exiv2 */

3d42617f249eb99fb15b4db14b43dca45eee3662
Sascha T. Ishikawa <sascha@zooniverse.org>
2016-01-26 22:46:03 -0800

    minor refactor and clean up messy callbacks

--- planet-api-before-after-test.js
+++ planet-api-before-after-test.js
@@ -9,7 +9,7 @@ var imgMeta      = require('./modules/image-meta.js')
 var csvStringify = require('csv-stringify')
 var uploadToS3   = require('./modules/upload-to-s3.js')
 var panoptesAPI  = require('./modules/panoptes-api.js')
-var parse        = require('csv-parse')
+var parseCsv     = require('csv-parse')
 
 /* Available mosaics:
  *   https://api.planet.com/v0/scenes/ortho/
@@ -31,14 +31,12 @@ var bounds = geoJSON.features[0].geometry.coordinates[0]
 /* Set parameters */
 var manifest_file = 'data/manifest.csv'
 var project_id     = '2035'
-var subject_set_id = '3614'
+var subject_set_id = '3616'
 var bucket = 'planetary-response-network'
 
-// /* Call Planet API and download GeoTIF and accompanying JSON files */
-// planetAPI.fetchMosaicFromAOI( bounds, before_url, 'foo')
-// planetAPI.fetchBeforeAndAfterMosaicFromAOI( before_url, after_url, bounds )
-
+/* Call Planet API and download GeoTIF and accompanying JSON files */
 planetAPI.fetchBeforeAndAfterMosaicFromAOI( before_url, after_url, bounds,
+  // process downloaded mosaics
   function (error, result){
     if(error){
       console.log(error);;
@@ -55,7 +53,7 @@ planetAPI.fetchBeforeAndAfterMosaicFromAOI( before_url, after_url, bounds,
       async.series( task_list, function(error, result) {
         console.log('Tilizing complete.');
         generateManifest( manifest_file, function(){
-          uploadImages(manifest_file, project_id, subject_set_id, function(){
+          deployPanoptesSubjects(manifest_file, project_id, subject_set_id, function(){
             // console.log('Finished uploading subjects.');
           })
         })
@@ -64,16 +62,19 @@ planetAPI.fetchBeforeAndAfterMosaicFromAOI( before_url, after_url, bounds,
   }
 )
 
-function uploadImages(manifest_file, project_id, subject_set_id, callback){
+// deployPanoptesSubjects(manifest_file, project_id, subject_set_id, function(){
+//   console.log('Finished uploading subjects.');
+// })
+
+function deployPanoptesSubjects(manifest_file, project_id, subject_set_id, callback){
   console.log('Uploading images...');
 
-  // Using the first line of the CSV data to discover the column names
-  rs = fs.readFile(manifest_file, function(err,data){
-    parse(data, {columns: true}, function(err,rows){
-
-      async.mapSeries(rows, uploadSubjectImagesToS3, function(error, result){
-        async.mapSeries(rows, createSubjectFromManifestRow, function(error, result){
-          panoptesAPI.saveSubjects(result, function(error, result){
+  // maybe clean up using async.waterfall?
+  fs.readFile(manifest_file, function(error,data){
+    parseCsv(data, {columns: true}, function(error,rows){
+      uploadImages(rows, function(error,rows){
+        generateSubjects(rows, function(error,subjects){
+          panoptesAPI.saveSubjects(subjects, function(error,result){
             if(error){
               callback(error)
             } else{
@@ -84,6 +85,27 @@ function uploadImages(manifest_file, project_id, subject_set_id, callback){
       })
     })
   })
+
+}
+
+function uploadImages(rows, callback){
+  async.mapSeries(rows, uploadSubjectImagesToS3, function(error,rows){
+    if(error){
+      callback(error)
+    } else{
+      callback(null,rows)
+    }
+  })
+}
+
+function generateSubjects(rows, callback){
+  async.mapSeries(rows, createSubjectFromManifestRow, function(error,subjects){
+    if(error){
+      callback(error)
+    } else{
+      callback(null,subjects)
+    }
+  })
 }
 
 // Note: assumes there are two valid images in each row
@@ -92,7 +114,6 @@ function uploadSubjectImagesToS3(row, callback){
     async.apply( uploadToS3, row['image1'], row['image1'], bucket ),
     async.apply( uploadToS3, row['image2'], row['image2'], bucket )
   ], function(error, result){
-    // console.log('Finished processing row! RESULT = ', result); // DEBUG
     // replace local filename with image url
     row['image1'] = result[0]
     row['image2'] = result[1]

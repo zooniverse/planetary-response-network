dc657fc811bb55b3b1268b2c579ef98fcd071126
Sascha T. Ishikawa <sascha@zooniverse.org>
2016-02-23 15:57:24 -0800

    copy lib and middleware (temporary); install missing dependency

---
+++ middleware/process-aoi.js
@@ -0,0 +1,13 @@
+'use strict'
+const path = require('path')
+const queue = require('../lib/queue')
+const UPLOAD_PATH = __dirname + '/../../uploaded_aois'
+
+module.exports = function (req, res, next) {
+  // Send job to queue
+  queue.push(path.join(UPLOAD_PATH, req.file.filename))
+
+  // Send confirmation
+  res.header('Content-Type', 'text/plain')
+  res.send('Upload complete, subject fetch job queued')
+}

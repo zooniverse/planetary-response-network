d8ca77f6d2d6b7dc96e06e75abeaff9b95d27821
Sascha T. Ishikawa <sascha@zooniverse.org>
2016-02-24 11:15:04 -0800

    ensure uploaded_aois directory exists

--- middleware/process-aoi.js
+++ middleware/process-aoi.js
@@ -2,17 +2,16 @@
 const fork = require('child_process').fork
 const path = require('path')
 const queue = require('../lib/queue')
-
 const UPLOAD_PATH = path.join(__dirname,'../uploaded_aois')
 
 module.exports = function (options) {
   return function (req, res, next) {
+    res.header('Content-Type', 'text/plain')
     if (options.useQueue) {
       // Send job to queue
       queue.push(path.join(UPLOAD_PATH, req.file.filename))
 
       // Send confirmation
-      res.header('Content-Type', 'text/plain')
       res.send('Upload complete, subject fetch job queued')
     } else {
       res.send('Upload complete, starting subject fetch job')

f71fd727b32d801c08ee0df0a6c575d05286309f
Tom Jenkins <tom@itsravenous.com>
2016-03-18 11:18:11 +0000

    Give manifest deploy responsiblilty

--- modules/manifest.js
+++ modules/manifest.js
@@ -3,39 +3,49 @@
 const async = require('async');
 const csvStringify = require('csv-stringify');
 const imgMeta = require('./image-meta');
+const panoptesAPI = require('./panoptes-api');
+const uploadToS3 = require('./upload-to-s3');
 
 class Manifest {
 
   /**
+   * @classdesc Manifests are responsible for generating subjects from mosaic files and uploading them to Panoptes
    * @param  {Array<Mosaic>}         mosaics        List of mosaics included in the manifest
-   * @param  {Array<Array<String>>}  filesByMosaic  List of mosaic file lists
+   * @param  {Aoi}                   aoi            An area of interest to use
+   * @param  {Number}                projectId      Id of Panoptes project to which subjects should be sent
+   * @param  {Number}                subjectSetId   Id of Panoptes subject set to which subjects should be sent
    */
-  constructor(mosaics, projectId, subjectSetId, filesByMosaic) {
+  constructor(mosaics, aoi, projectId, subjectSetId) {
     this.mosaics = mosaics;
-    this.filesByMosaic = filesByMosaic;
+    this.aoi = aoi;
     this.projectId = projectId;
     this.subjectSetId = subjectSetId;
   }
 
   /**
-   * Generates the manifest rows, each with one image from each mosaic. This assumes that the tiles in each mosaic are in order and of the same geographic region
+   * Generates the manifest subjects, each with one image from each mosaic. This assumes that the tiles in each mosaic are in order and of the same geographic region
    * @param  {Function}  callback
    */
   getSubjects(callback) {
-    var tasks = [];
-    var fileTuples = [];
-    var i = 0;
-    for (var file of this.filesByMosaic[0]) {
-      let tuple = [];
-      for (var mosaicFiles of this.filesByMosaic) {
-        tuple.push(mosaicFiles[i]);
+    // Fetch and tile imagery from mosaics
+    async.mapSeries(this.mosaics, (mosaic, callback) => {
+      mosaic.createTilesForAOI(this.aoi, callback);
+    }, (err, filesByMosaic) => {
+      if (err) throw err;
+      var tasks = [];
+      var fileSets = [];
+      for (var i = 0; i < filesByMosaic[0].length; i++) {
+        let fileSet = [];
+        for (var mosaicFiles of filesByMosaic) {
+          fileSet.push(mosaicFiles[i]);
+        }
+        fileSets.push(fileSet);
+        tasks.push(async.apply(this.getSubject.bind(this), fileSet));
+        i++;
       }
-      fileTuples.push(tuple);
-      tasks.push(async.apply(this.getSubject.bind(this), tuple));
-      i++;
-    }
-    async.series(tasks, (err, subjects) => {
-      callback(err, subjects);
+      async.series(tasks, (err, subjects) => {
+        callback(err, subjects);
+      });
     });
   }
 
@@ -64,6 +74,33 @@ class Manifest {
         callback(e);
       }
     })
+  }
+
+  deploy(callback) {
+    async.waterfall([
+      this.getSubjects.bind(this),
+      this.uploadSubjectsImages.bind(this),
+      panoptesAPI.saveSubjects
+    ], callback);
+  }
+
+  uploadSubjectsImages(subjects, callback){
+    console.log('Uploading images...');
+    async.mapSeries(subjects, this.uploadSubjectImagesToS3.bind(this), callback);
+  }
+
+  uploadSubjectImagesToS3(subject, callback){
+    async.series(this.mosaics.map((mosaic, i) => {
+      const img = subject.locations[i]['image/jpeg'];
+      return async.apply( uploadToS3, img, img, process.env.AMAZON_S3_BUCKET );
+    }), function(err, results) {
+      if (err) return callback(err)
+      // replace local filename with image url
+      results.forEach((result, i) => {
+        subject.locations[i]['image/jpeg'] = result;
+      });
+      callback(null, subject);
+    });
   }
 
 }

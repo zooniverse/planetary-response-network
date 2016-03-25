5bb656a06cfb3633da219cde89e2191ceb887678
Sascha T. Ishikawa <sascha@zooniverse.org>
2016-03-25 11:18:57 -0700

    towards handling sentinel-2 data

--- generate-subjects.js
+++ generate-subjects.js
@@ -5,6 +5,10 @@ const Mosaic        = require('./modules/mosaic');
 const Manifest      = require('./modules/manifest');
 const Status        = require('./modules/status');
 
+// for Sentinel-2 data
+const utmObj = require('utm-latlng');
+const mgrs   = require('mgrs');
+
 // Go
 const argv = yargs
   .usage('$0 [options] <kml_file>')
@@ -14,7 +18,7 @@ const argv = yargs
   .describe('project',       'ID of target project')
   .describe('subject-set',   'ID of target subject set')
   .default('provider',       'planet-api')
-  .choices('provider',       ['planet-api'])
+  .choices('provider',       ['planet-api', 'sentinel-2'])
   .demand([
       'project',
       'subject-set'
@@ -38,15 +42,70 @@ const redisHost = {
 const status = new Status(argv.jobId, redisHost);
 
 // Create mosaic instances from provided URLs
-const mosaics = argv.mosaics.map((mosaic, i) => {
-  return new Mosaic(argv.provider, 'image' + (i + 1), mosaic, status);
-});
+switch (argv.provider){
+  case 'planet-api':
+    console.log('Using mosaic provider \'%s\'', argv.provider);
+    fetchPlanetData();
+    break;
+  case 'sentinel-2':
+    console.log('Using mosaic provider \'%s\'', argv.provider);
+    fetchSentinelData(aoi.bounds);
+    break;
+  default:
+    console.log('ERROR: Invalid provider \'%s\'', argv.provider);
+}
 
-// Create and upload subjects
-const manifest = new Manifest(mosaics, aoi, argv.project, argv.subjectSet, status);
+// Fetches data from Planet Labs API
+function fetchPlanetData() {
+  const mosaics = argv.mosaics.map((mosaic, i) => {
+    return new Mosaic(argv.provider, 'image' + (i + 1), mosaic, status);
+  });
 
-manifest.deploy((err, result) => {
-  if (err) throw err;
-  this.status.update('deploying_subjects', 'done');
-  console.log('Finished uploading subjects.');
-});
+  // Create and upload subjects
+  const manifest = new Manifest(mosaics, aoi, argv.project, argv.subjectSet, status);
+
+  manifest.deploy((err, result) => {
+    if (err) throw err;
+    this.status.update('deploying_subjects', 'done');
+    console.log('Finished uploading subjects.');
+  });
+}
+
+// Fetches data from Sinergise/ESA Sentinel 2 API
+
+function fetchSentinelData(bounds) {
+  console.log('Fetching Sentinel-2 data...');
+  console.log('BOUNDS: ', bounds);
+
+  for(let coord of bounds) {
+    console.log('------------------------');
+    console.log('COORD: ', coord);
+    console.log('MGRS : ', mgrs.forward(coord));
+  }
+
+  process.exit(0);
+}
+
+
+
+
+// const AWS    = require('aws-sdk');
+// const utmObj = require('utm-latlng');
+//
+//
+// var s3 = new AWS.S3({
+//   region: 'eu-central-1',
+//   params: { Bucket: 'sentinel-s2-l1c' }
+// });
+//
+// s3.listObjects( { Prefix: 'tiles', Marker: '.jp2' }, function(err, data) {
+//   if (err) console.log(err, err.stack);
+//   else {
+//     console.log(data);
+//   }
+// });
+//
+//
+// // var utm=new utmObj('WGS 84');
+// // var coords = utm.convertUtmToLatLng(300000.0, 2990220.0 , 45, 'R')
+// // console.log('COORDINATES: ', coords.lat, coords.lng )

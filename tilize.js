93e78957c0189a663770de6c7b89481db3d91ac0
Tom Jenkins <tom@itsravenous.com>
2016-05-04 18:05:08 +0100

    Add label and equalize options Add callback with created tiles list

--- tilize.js
+++ tilize.js
@@ -1,3 +1,4 @@
+'use strict';
 const yargs = require('yargs');
 const tilize = require('./modules/tilize-image');
 
@@ -11,11 +12,29 @@ const argv = yargs
     describe: 'Tile overlap (both x and y)',
     default: 250
   })
+  .option('equalize', {
+    describe: 'Equalize histogram to stretch contrast',
+    boolean: true
+  })
+  .option('labels', {
+    describe: 'Space-separated list of labels to overlay on tiles (first label will be used for tiles from first image, and so on)',
+    array: true
+  })
   .demand(1)
   .argv;
 
+let options = {};
+if (argv.labels) options.labels = argv.labels;
+if (argv.equalize) options.equalize = argv.equalize;
+
+function done (err, files) {
+  if (err) throw err;
+  console.log('Done, created tiles:')
+  console.log(files.join('\n'));
+}
+
 if (argv._.length === 1) {
-  tilize.tilize(argv._[0], argv.size, argv.overlap);
+  tilize.tilize(argv._[0], argv.size, argv.overlap, options, done);
 } else {
-  tilize.tilizeMany(argv._, argv.size, argv.overlap);
+  tilize.tilizeMany(argv._, argv.size, argv.overlap, options, done);
 }

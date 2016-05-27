36471cf0d72b4b402bbda27f2de1c9759fcb6518
Sascha T. Ishikawa <sascha@zooniverse.org>
2016-05-27 12:59:13 -0700

    clean up

--- config.js
+++ config.js
@@ -19,19 +19,16 @@ var PRN_CLIENTS = {
 // determine environment
 var envFromShell = process.env.NODE_ENV;
 var env = envFromShell || DEFAULT_ENV;
-console.log('ENV = ', env);
 
 // set client
 var clientFromShell = process.env.PRN_CLIENT;
 var client = clientFromShell || PRN_CLIENTS[env];
 console.log('USING PRN CLIENT: ', client); // DEBUG CODE
 
-
 // set host
 var hostFromShell = process.env.PRN_HOST;
 var host = hostFromShell || PRN_HOSTS[env];
 console.log('USING PRN HOST: ', host); // DEBUG CODE
-
 
 if (!env.match(/^(production|staging|development)$/)) {
   throw new Error('Planetary Response Network (client): Invalid Environment; ' +
